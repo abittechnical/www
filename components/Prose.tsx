8a99be1a72b9d83434149b10ddf237b3901ab12f
George Bockari <bitwhys@gmail.com>
2024-06-19 18:41:13 -0400

    fix: fix tailwind class-order issue

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
 
 export function Prose({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
-  return <div className={clsx(className, 'prose prose-headings:font-heading dark:prose-invert')} {...props} />
+  return <div className={clsx(className, 'prose dark:prose-invert prose-headings:font-heading')} {...props} />
 }
