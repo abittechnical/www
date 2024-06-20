208c30f5047b979876624e91ec0530264a2e9694
George Bockari <bitwhys@gmail.com>
2024-06-19 20:48:08 -0400

    refactor(button): minor color changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import clsx from 'clsx'
 
+// TODO: convert to cva component, fix colors
 const variantStyles = {
   primary:
-    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
+    'bg-[--olive-8] font-semibold text-[--olive-12] hover:bg-[--olive-9] active:bg-[--olive-8] active:text-zinc-100/70',
   secondary:
-    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
+    'bg-[--olive-3] font-medium text-body hover:text-heading hover:bg-[--olive-5] active:bg-[--olive-4] active:text-heading',
 }
 
 type ButtonProps = {
