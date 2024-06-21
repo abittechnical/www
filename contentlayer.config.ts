00f734d27c67e8cc8a2f5e525e5284b3fea27b22
George Bockari <bitwhys@gmail.com>
2024-06-20 21:41:03 -0400

    chore: comment out remarkGfm plugin for further evaluation, update article source fields

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,25 +1,30 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 // import rehypePrism from '@mapbox/rehype-prism'
-import remarkGfm from 'remark-gfm'
+// import remarkGfm from 'remark-gfm'
 
 export const Article = defineDocumentType(() => ({
   name: 'Article',
+  // matches all `.mdx` files in the `articles` directory
   filePathPattern: `**/*.mdx`,
   contentType: 'mdx',
   fields: {
+    // currently matches "frontmatter" of the sample articles from the template
+    author: { type: 'string', required: true },
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
+    description: { type: 'string', required: true },
   },
   computedFields: {
-    url: { type: 'string', resolve: post => `/articles/${post._raw.flattenedPath}` },
+    slug: { type: 'string', resolve: post => `/articles/${post._raw.flattenedPath}` },
   },
 }))
 
 export default makeSource({
-  mdx: {
-    remarkPlugins: [remarkGfm],
-    // rehypePlugins: [highlight],
-  },
+  // remove for now until we understand its benefits
+  // mdx: {
+  //   remarkPlugins: [remarkGfm],
+  //   // rehypePlugins: [highlight],
+  // },
   contentDirPath: 'articles',
   documentTypes: [Article],
 })
